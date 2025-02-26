c001ae00ae84769b2ea0dc2ffab04517ddbe164d
Srisanjaykumar M <srisanjaykumar2000@gmail.com>
2025-02-26 22:08:54 +0530

    created viewmodel class and UIstate data class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ android {
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
